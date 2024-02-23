07716b01505a86f89db7857cb5b9c0d625faa68b
luissoto7710 <luissoto7710@gmail.com>
2024-02-23 14:27:12 -0400

    My Commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -8,7 +8,9 @@
 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
 }
 

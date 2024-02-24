ac123b65e382c959c9e4428a737d9813efb2cbff
luissoto7710 <luissoto7710@gmail.com>
2024-02-24 13:55:39 -0400

    My Commit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-      int main () 
-      {
-      printf("with proper grammar, but the outcome is a piece of art,");
-      return 0;
+/**
+ * main - prints the text inside closed captions
+ *
+ * Return: it returns (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

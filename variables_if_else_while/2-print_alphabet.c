9289c5c13102ea52f5ccbfa854ad60ac67f710af
luissoto7710 <154378919+luissoto7710@users.noreply.github.com>
2024-02-24 21:45:03 -0500

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - alphabet
+*
+* Return: always (0)
+*/
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+
+	{
+		putchar(abc);
+
+	}
+		putchar('\n');
+
+return (0);
+}

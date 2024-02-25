c8c18436d78f8b22c2d01b8bd860dbbc5dac0f74
luissoto7710 <154378919+luissoto7710@users.noreply.github.com>
2024-02-24 21:59:31 -0500

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - commas
+*
+*
+*Return: (0)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+
+			putchar(' ');
+
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

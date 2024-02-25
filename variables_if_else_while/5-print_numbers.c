501ab17f67f1d4b17afd2a9daed0dc4426ff8316
luissoto7710 <154378919+luissoto7710@users.noreply.github.com>
2024-02-24 21:53:53 -0500

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - numbers
+*
+* Return: always (0)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+
+	{
+		putchar(num);
+
+	}
+		putchar('\n');
+
+return (0);
+}

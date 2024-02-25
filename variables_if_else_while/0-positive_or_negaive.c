48ad71d5ac210253050e7ac9b605bd13f5e5e0b8
luissoto7710 <luissoto7710@gmail.com>
2024-02-24 21:24:58 -0400

    My Commit

---
+++ variables_if_else_while/0-positive_or_negaive.c
@@ -0,0 +1,31 @@
+#include <stfio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *main - main function
+ *
+ *Description:
+ *
+ *Return: Always return 0
+ */
+int main (void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+	return (0);
+}

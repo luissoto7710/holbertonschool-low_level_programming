1091d46c4cd7510e89432fafca46593d36828277
luissoto7710 <154378919+luissoto7710@users.noreply.github.com>
2024-02-24 21:55:55 -0500

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - leterrrrs
+*
+* Return: always (0)
+*/
+int main(void)
+{
+	char abc;
+
+	for (abc = 'z'; abc >= 'a'; abc--)
+
+	{
+		putchar(abc);
+
+	}
+		putchar('\n');
+
+return (0);
+}

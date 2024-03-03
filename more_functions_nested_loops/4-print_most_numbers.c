23ae5b551d16f11442d64a8a5412734c98cbe959
luissoto7710 <luissoto7710@gmail.com>
2024-03-02 21:57:07 -0400

    My Commit

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers 0 to 9 follwed by new line with the exception of 2 and 4
+ * print_most_numbers - prints the numbers 0 to 9 
+ * follwed by new line with the exception of 2 and 4
  *
  * Return: 0
  */

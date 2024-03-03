4577731a7fd1c468812839318969fcca9d4ca2fa
luissoto7710 <luissoto7710@gmail.com>
2024-03-02 22:05:14 -0400

    My Commit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,29 @@
 #include <stdio.c>
 #include "main.c"
 
+/**
+ * more numbers - prints numbers 0 - 14
+ * 10 times
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+
+	char j;
+
+	do {
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		i++;
+		_putchar('\n');
+	} while (i <= 9);
+}

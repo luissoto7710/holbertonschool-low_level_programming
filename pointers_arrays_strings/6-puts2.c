efe35a13b7afd30a6329e5dbaf32a5806fe65cc3
luissoto7710 <luissoto7710@gmail.com>
2024-03-02 23:48:56 -0400

    My Commit

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - description
+ *
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}

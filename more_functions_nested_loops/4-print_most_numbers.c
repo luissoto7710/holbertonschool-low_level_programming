3a8d4117b78e3ff6119e643875c563db8ff7217e
luissoto7710 <luissoto7710@gmail.com>
2024-03-02 21:46:56 -0400

    My Commit

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers 0 to 9 follwed by new line with the exception of 2 and 4
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int x = '2';
+	int y = '4';
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		if (num != x && num != y)
+		{
+		_putchar(num);
+		}
+			_putchar('\n');
+}

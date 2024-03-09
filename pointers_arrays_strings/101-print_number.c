5edd64003e1d049535c98f66d4faff39342c6dfd
luissoto7710 <luissoto7710@gmail.com>
2024-03-09 19:04:57 -0400

    My Commit

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - desc
+ * @n: desc
+ * Return: succ
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}

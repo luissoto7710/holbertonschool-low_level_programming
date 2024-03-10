ecf1bc91fc5b4d155cf793b70ae3a630cb584cc3
luissoto7710 <luissoto7710@gmail.com>
2024-03-09 21:14:05 -0400

    My Commit

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memset - desc
+ * @s: desc
+ * @b: desc
+ * @n: desc
+ * Return: suc
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    for (unsigned int i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
+
+    return (s);
+}

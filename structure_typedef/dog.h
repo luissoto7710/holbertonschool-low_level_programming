15d840a31093b8e93429efc6ef88e4e89e743182
luissoto7710 <luissoto7710@gmail.com>
2024-03-16 22:30:57 -0400

    My Commit

---
+++ structure_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - Struct dog
+ *@name: Name
+ *@age: Age
+ *@owner: Owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ *dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */

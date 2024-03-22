4dac8d0424d0c127471f507229dedb6eba125195
luissoto7710 <luissoto7710@gmail.com>
2024-03-22 14:20:45 -0400

    My Commit

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - array iterator
+ * @array: the array
+ * @size: size of array
+ *
+ */
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+if (array == NULL || action == NULL)
+	return;
+while (size-- > 0)
+{
+	action(*array);
+	array++;
+}
+}

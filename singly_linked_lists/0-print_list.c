fa13b337c95f47341be37ad91b04076afccdf10b
luissoto7710 <luissoto7710@gmail.com>
2024-04-06 18:20:46 -0400

    My Commit

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,24 +3,24 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list_t list
- * 
+ *
  * Return: the number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
-    const list_t *current = h;
-    size_t nodes = 0;
+   const list_t *current = h;
+   size_t nodes = 0;
 
-    while (current != NULL)
-    {
-        if (current->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", current->len, current->str);
-        current = current->next;
-        nodes++;
-    }
+   while (current != NULL)
+   {
+       if (current->str == NULL)
+           printf("[0] (nil)\n");
+       else
+           printf("[%u] %s\n", current->len, current->str);
+       current = current->next;
+       nodes++;
+   }
 
-    return nodes;
+   return nodes;
 }
 

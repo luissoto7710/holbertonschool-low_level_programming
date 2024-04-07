bf5ac1933accd8dd6e9030f48f8bcebf44614434
luissoto7710 <luissoto7710@gmail.com>
2024-04-06 23:37:33 -0400

    My Commit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,18 +15,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int new_file, len = 0, wr_stat;
+   int new_file, len = 0, wr_stat;
 
-    if (!filename)
-        return (-1);
+   if (!filename)
+       return (-1);
 
-    new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (new_file == -1)
-        return (-1);
+   new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+   if (new_file == -1)
+       return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
+   if (text_content != NULL)
+   {
+       while (text_content[len])
             len++;
 
         wr_stat = write(new_file, text_content, len);

e19f75f7d724e8acd4b88504536a432a114b7ecf
luissoto7710 <luissoto7710@gmail.com>
2024-04-06 23:09:17 -0400

    My Commit

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to the name of the file
+ * @letters: number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print,
+ * 0 if the file cannot be opened or read, filename is NULL,
+ * or if write fails or does not write the expected amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    ssize_t n_read, n_written;
+    char *buffer;
+
+    if (!filename)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    buffer = malloc(sizeof(char) * (letters));
+    if (!buffer)
+    {
+        close(fd);
+        return (0);
+    }
+
+    n_read = read(fd, buffer, letters);
+    if (n_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    n_written = write(STDOUT_FILENO, buffer, n_read);
+    if (n_written == -1 || n_written != n_read)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    free(buffer);
+    close(fd);
+    return (n_read);
+}
+

d8e7b094cbe5fda8dba1893c52ee7b4f9c901fe3
luissoto7710 <luissoto7710@gmail.com>
2024-04-06 22:41:20 -0400

    My Commit

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -17,17 +17,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 
 	if (!b)
-		return 0;
+		return (0);
 
 	while (*b) {
 		num <<= 1;
 		if (*b == '1')
 			num += 1;
 		else if (*b != '0')
-			return 0;
+			return (0);
 		b++;
 	}
 
-	return num;
+	return (num);
 }
 

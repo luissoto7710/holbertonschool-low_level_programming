84593aeb651f0aef9c7f1761fe0ce4b27cc19dd8
luissoto7710 <luissoto7710@gmail.com>
2024-04-06 23:43:38 -0400

    My Commit

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,27 +9,27 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    int bytes_written;
+int fd;
+int bytes_written;
 
-    if (!filename)
-        return (-1);
+if (!filename)
+return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
 
-    if (text_content)
-    {
-        bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+if (text_content)
+{
+bytes_written = write(fd, text_content, strlen(text_content));
+if (bytes_written == -1)
+{
+close(fd);
+return (-1);
+}
+}
 
-    close(fd);
-    return (1);
+close(fd);
+return (1);
 }
 
